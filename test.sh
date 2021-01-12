98b560d7a9354af1192bfc0d4c2daf7e6cd6343f
Brendan Morrell <brendanmorrell@gmail.com>
2021-01-11 19:19:06 -0800

    $default_message_start$branch

---
+++ test.sh
@@ -0,0 +1,16 @@
+f(){
+  git stash-all;
+  branch=$(git rev-parse --abbrev-ref HEAD);
+  date=$(date "+%m-%d-%y_%H.%M");
+  spacer="_(stash_";
+  name="$branch$spacer$date";
+  name+=")";
+  git cob $name;
+  git stash apply;
+  message_start="Savepoint stash push for ";
+  full_message="$message_start$branch";
+  git cm $full_message;
+  git pocnv;
+  git co -;
+  git stash apply;
+};f;
